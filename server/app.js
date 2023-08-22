e75debf13b77e68d0094abccf216f8a3c217e260
shizanatorionlabs <142798331+shizanatorionlabs@users.noreply.github.com>
2023-08-22 11:08:28 +0530

    New app.js

--- server/app.js
+++ server/app.js
@@ -4,12 +4,8 @@ import path from 'path';
 const app = express();
 const port = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, '../client/dist')));
-
 app.get('/', (req, res) => {
   res.end('Wassup');
-  // You might want to use res.sendFile here if you want to serve an HTML file
-  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
 app.listen(port, () => {
